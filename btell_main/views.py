aa01ebba5e7850c9c7d415888d7d75aaacfd8c92
Marko Zagar <zagar@evil.si>
2023-04-28 18:13:21 +0100

    Switch to using compiled bootstrap SCSS, replace index page

--- btell_main/views.py
+++ btell_main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django import http
+from django import http, shortcuts
 
 
 def index(request: http.HttpRequest) -> http.HttpResponse:
-    return http.HttpResponse('IT WORKS MAYBE')
+    context = {}
+    return shortcuts.render(request, 'btell_main/index.html', context)
