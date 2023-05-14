33af858f6d8522692305ad929b67194fa31ec00a
Fapso <adamjaworowski@o2.pl>
2023-05-14 14:31:25 +0200

    stubs of custom login in and registration pages; and their functions as Django views; had to silence three type errors

--- btell_main/urls.py
+++ btell_main/urls.py
@@ -3,9 +3,13 @@ from django.urls import path
 
 from btell_main.views import index as index_view
 
+from btell_main.forms import login, register
+
+
 urls = [
     path('', index_view.index, name='btell_index'),
     # Authentication views
-    path('a/login/', auth_views.LoginView.as_view(template_name='btell_main/accounts/login.html', next_page='/'), name='login'),
+    path('a/login.html/', login.LoginPage.as_view(), name='login'),
+    path('a/register.html/', register.RegisterPage.as_view(), name='register'),
     path('a/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
