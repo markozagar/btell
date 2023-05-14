97680330c48f91c2911109d8ee329a4cb188f44e
Fapso <adamjaworowski@o2.pl>
2023-05-14 14:31:25 +0200

    stubs of custom login in and registration pages; and their functions as Django views; had to silence three type errors

--- btell_main/views/index.py
+++ btell_main/views/index.py
@@ -3,12 +3,14 @@ from django import forms, http
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 
+from typing import Dict, Any
+
 from django.shortcuts import render, redirect
 
 from btell_main.views import context as btell_context
 
 
 def index(request: http.HttpRequest) -> http.HttpResponse:
-    ctx = {}
+    ctx: Dict[str, Any] = {}
     btell_context.context_add_user_info(request, ctx)
     return render(request, 'btell_main/index.html', ctx)
