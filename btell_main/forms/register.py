4377d417856c4ee76a1faf2474c39a529aa02dd3
Fapso <adamjaworowski@o2.pl>
2023-05-14 16:08:58 +0200

    Suposedly fixed what was pointed out as not in style.

--- btell_main/forms/register.py
+++ btell_main/forms/register.py
@@ -1,13 +1,11 @@
-# from btell_main.models import Profile
-from django.views.generic.edit import FormView
-from django.shortcuts import redirect
-from django.urls import reverse_lazy
+from django import shortcuts
+from django import urls
 from django import forms
-from django.core.validators import validate_email
-from django.contrib.auth import authenticate, login
-# from django.contrib.auth import views as auth_views
+from django.contrib import auth
 from django.contrib.auth import models as auth_models
 from django.contrib.auth import forms as auth_forms
+from django.views.generic.edit import FormView
+from django.core.validators import validate_email
 
 # from datetime import datetime, timezone
 
@@ -89,24 +87,18 @@ class Register_User(auth_forms.UserCreationForm):
         #     self.add_error('password2', "Passwords are not identical.")
 
         cook = False
-        print(f"cookies is = {cook}")
         if 'accepted_cookies' in data:
             cook = data['accepted_cookies']
-            # cook = True
 
-        if cook is not True:
-            print(f"cookies is = {cook}")
-            self.add_error('accepted_cookies', "Cookies Policy not accepted.")
+        # if cook is not True:
+        #     self.add_error('accepted_cookies', "Cookies Policy not accepted.")
 
         tos = False
-        print(f"tos is = {tos}")
         if 'accepted_tos' in data:
             tos = data['accepted_tos']
-            # tos = True
 
-        if tos is not True:
-            print(f"tos is = {tos}")
-            self.add_error('accepted_tos', "Terms of Use not accepted.")
+        # if tos is not True:
+        #     self.add_error('accepted_tos', "Terms of Use not accepted.")
 
         return data
 
@@ -124,11 +116,11 @@ class RegisterPage(FormView):
     template_name = 'btell_main/accounts/registration.html'
     form_class = Register_User
     redirect_authenticated_user = True
-    success_url = reverse_lazy('btell_index')
+    success_url = urls.reverse_lazy('btell_index')
 
     def get(self, request, *args, **kwargs):
         if self.request.user.is_authenticated:
-            return redirect('btell_index')
+            return shortcuts.redirect('btell_index')
         return super().get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
@@ -145,6 +137,6 @@ class RegisterPage(FormView):
         # Profile manipulation if any should be here, or in the models
 
         # Logging
-        user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
-        login(self.request, user)
-        return redirect('btell_index')
+        user = auth.authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
+        auth.login(self.request, user)
+        return shortcuts.redirect('btell_index')
