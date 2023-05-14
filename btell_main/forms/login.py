6fb33f4346b0b598c1a79f7ae43ba9605edbe06f
Fapso <adamjaworowski@o2.pl>
2023-05-14 16:08:58 +0200

    Suposedly fixed what was pointed out as not in style.

--- btell_main/forms/login.py
+++ btell_main/forms/login.py
@@ -1,9 +1,8 @@
+from django.contrib import auth
 from django.contrib.auth import forms as auth_forms
 from django.contrib.auth import views as auth_views
-from django.contrib.auth import authenticate, login
 
-from django import forms
-from django.urls import reverse_lazy
+from django import urls
 
 from django.shortcuts import redirect
 
@@ -11,7 +10,7 @@ from django.shortcuts import redirect
 class LoginPage(auth_views.LoginView):
     template_name = 'btell_main/accounts/login.html'
     form_class = auth_forms.AuthenticationForm
-    success_url = reverse_lazy('btell_index')
+    success_url = urls.reverse_lazy('btell_index')
     redirect_authenticated_user = True
 
     def get_context_data(self, **kwargs):
@@ -24,6 +23,6 @@ class LoginPage(auth_views.LoginView):
         return super().form_invalid(form)  # type: ignore # it works, but shows an error
 
     def form_valid(self, form):
-        user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
-        login(self.request, user)
+        user = auth.authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+        auth.login(self.request, user)
         return redirect('btell_index')
